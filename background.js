77108336f6d07cc39201868e44d37e0c34dbede2
Decklin Foster <decklin@red-bean.com>
2010-02-01 02:57:48 -0500

    Refactor

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
-function b64hmac(message, secret) {
-    var bytes = Crypto.HMAC(Crypto.SHA256, message, secret, {asBytes: true});
+function hmacSHA256(message, secret) {
+    return Crypto.HMAC(Crypto.SHA256, message, secret, {asBytes: true});
+}
+
+function base64(bytes) {
     return Crypto.util.bytesToBase64(bytes);
 }
 
-function first8(str) {
-    return str.replace(/[^0-9A-Za-z]/g, '').substr(0, 8);
+function alnum(s) {
+    return s.replace(/[^0-9A-Za-z]/g, '');
 }
 
 chrome.extension.onRequest.addListener(function(msg, src, send) {
@@ -18,7 +21,7 @@ chrome.extension.onRequest.addListener(function(msg, src, send) {
         });
     }
     if (msg.domain && msg.secret) {
-        send(first8(b64hmac(msg.domain, msg.secret)));
+        send(alnum(base64(hmacSHA256(msg.domain, msg.secret))).substr(0, 8));
     }
 });
 
--- macguffin.py
+++ macguffin.py
@@ -7,18 +7,18 @@ import hashlib
 import hmac
 import getpass
 
-def first8(s):
-    return re.sub('[^A-Za-z0-9]', '', s)[:8]
+def alnum(s):
+    return re.sub('[^A-Za-z0-9]', '', s)
 
-def b64hmac(msg, key):
-    return base64.b64encode(hmac.new(key, msg, hashlib.sha256).digest())
+def hmac_sha256(msg, key):
+    return hmac.new(key, msg, hashlib.sha256).digest()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print >>sys.stderr, 'usage: macguffin HOSTNAME'
         sys.exit(1)
 
-    hostname = sys.argv[1]
+    domain = sys.argv[1]
     key = getpass.getpass('Passphrase: ')
 
-    print first8(b64hmac(hostname, key))
+    print alnum(base64.b64encode(hmac_sha256(domain, key)))[:8]
