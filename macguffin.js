9fcfb66e19c190dac1e762d19ceae4a9228bbd79
Decklin Foster <decklin@red-bean.com>
2010-02-04 13:40:15 -0500

    Oh alright add a keyboard binding for now

--- macguffin.js
+++ macguffin.js
@@ -1,5 +1,6 @@
 var animLen = 250;
 var focused = null;
+var hotKey = 120;
 
 function domain(hostname) {
     var last = function(n, a) { return a.slice(a.length - n, a.length); };
@@ -7,6 +8,27 @@ function domain(hostname) {
     var parts = hostname.split('.');
     var n = last(2, parts).every(short) ? 3 : 2;
     return last(n, parts).join('.');
+}
+
+function genPassword() {
+    var secretLen = focused.value.length;
+    var animStep = animLen / (secretLen + 8);
+    var queueSubstr = function(s, i) {
+        $(focused).delay(animStep).queue(function() {
+            $(this).val(s.substr(0, i + 1));
+            $(this).dequeue();
+        });
+    }
+    var req = {
+        domain: domain(location.hostname),
+        secret: focused.value
+    };
+    chrome.extension.sendRequest(req, function(hmac) {
+        for (var i = secretLen - 1; i > 0; i--)
+            queueSubstr(focused.value, i);
+        for (var i = 0; i < 8; i++)
+            queueSubstr(hmac, i);
+    });
 }
 
 $('input[type="password"]').removeAttr('maxlength').focus(function() {
@@ -20,27 +42,13 @@ $('input[type="password"]').removeAttr('maxlength').focus(function() {
     chrome.extension.sendRequest({
         pageAction: 'hide'
     });
+}).keydown(function(event) {
+    console.log(event);
+    if (event.keyCode === hotKey)
+        genPassword();
 });
 
 chrome.extension.onRequest.addListener(function(msg, src, send) {
-    if (focused && msg.clicked) {
-        var secretLen = focused.value.length;
-        var animStep = animLen / (secretLen + 8);
-        var queueSubstr = function(s, i) {
-            $(focused).delay(animStep).queue(function() {
-                $(this).val(s.substr(0, i + 1));
-                $(this).dequeue();
-            });
-        }
-        var req = {
-            domain: domain(location.hostname),
-            secret: focused.value
-        };
-        chrome.extension.sendRequest(req, function(hmac) {
-            for (var i = secretLen - 1; i > 0; i--)
-                queueSubstr(focused.value, i);
-            for (var i = 0; i < 8; i++)
-                queueSubstr(hmac, i);
-        });
-    }
+    if (focused && msg.clicked)
+        genPassword();
 });
